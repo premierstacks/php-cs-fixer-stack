07727d7b480dd39425ba9f07aeb034db7ee49036
Tomáš Chochola <chocholatom1997@gmail.com>
2024-04-03 18:06:50 +0200

    Code style fix

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -3,13 +3,16 @@
 /**
  * Copyright © 2024+ Tomáš Chochola <chocholatom1997@gmail.com> - All Rights Reserved
  *
- * This software is the proprietary property of Tomáš Chochola and is protected by copyright laws.
- * Despite its potential public availability, it remains proprietary and not free or open-source.
- * A valid license is required for any use of the software.
+ * This software is the exclusive property of Tomáš Chochola, protected by copyright laws.
+ * Although the source code may be accessible, it is not free for use without a valid license.
+ * A valid license, obtainable through proper channels, is required for any software use.
+ * For licensing or inquiries, please contact Tomáš Chochola or refer to the GitHub Sponsors page.
  *
- * The complete license terms are detailed in the LICENSE.md file within the source code.
+ * The full license terms are detailed in the LICENSE.md file within the source code repository.
+ * The terms are subject to changes. Users are encouraged to review them periodically.
  *
- * For license acquisition or inquiries, please contact Tomáš Chochola or visit the GitHub Sponsors page:
+ * The Proprietor: Tomáš Chochola
+ * - Role: The Creator, Proprietor & Project Visionary
  * - Email: chocholatom1997@gmail.com
  * - GitHub: https://github.com/tomchochola
  * - Sponsor & License: https://github.com/sponsors/tomchochola
